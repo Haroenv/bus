0ea1de68092a4380b6ad971e896316fe59dd5e9b
Haroen Viaene <hello@haroen.me>
2016-02-07 22:30:43 +0100

    interactive

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
 ---
 # comment for jekyll
 ---
+
+var stopButton = document.getElementsByClassName('tracker__stop')[0];
+stopButton.addEventListener('click',function(){
+  this.classList.toggle('tracker__stop--enquired');
+});
