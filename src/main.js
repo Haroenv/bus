c54c02e2e4d7aab22b61a3719ef5f4bdddf0a8e9
Haroen Viaene <hello@haroen.me>
2016-02-07 22:54:06 +0100

    sound

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@
 # comment for jekyll
 ---
 
+var notification = document.createElement('audio');
+notification.src = 'src/notification.mp3'
+
 var stopButton = document.getElementsByClassName('tracker__stop')[0];
 stopButton.addEventListener('click',function(){
   this.classList.toggle('tracker__stop--enquired');
+  // play sound
+  notification.play();
 });
